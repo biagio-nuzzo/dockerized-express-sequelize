d77f1e4360c4f59b77e1e34ec8c473ac1f30383a
Biagio Nuzzo <57946383+biagio-nuzzo@users.noreply.github.com>
2023-06-28 18:31:19 +0200

    comment users model

--- models/users.js
+++ models/users.js
@@ -1,27 +1,32 @@
-const Sequelize = require("sequelize");
-const db = require("../util/database");
+const Sequelize = require("sequelize"); // Import the Sequelize library
+const db = require("../util/database"); // Import the Sequelize instance
 
 const User = db.define("user", {
+  // Define a model named "user" with the specified attributes
   id: {
-    type: Sequelize.INTEGER,
-    autoIncrement: true,
-    allowNull: false,
-    primaryKey: true,
+    // Attribute for the user's ID
+    type: Sequelize.INTEGER, // Data type is INTEGER
+    autoIncrement: true, // ID value is automatically incremented
+    allowNull: false, // ID cannot be null
+    primaryKey: true, // ID is the primary key of the model
   },
   username: {
-    type: Sequelize.STRING,
-    allowNull: false,
-    unique: true,
+    // Attribute for the user's username
+    type: Sequelize.STRING, // Data type is STRING
+    allowNull: false, // Username cannot be null
+    unique: true, // Username must be unique
   },
   email: {
-    type: Sequelize.STRING,
-    allowNull: true,
-    unique: true,
+    // Attribute for the user's email
+    type: Sequelize.STRING, // Data type is STRING
+    allowNull: true, // Email can be null
+    unique: true, // Email must be unique
   },
   password: {
-    type: Sequelize.STRING,
-    allowNull: false,
+    // Attribute for the user's password
+    type: Sequelize.STRING, // Data type is STRING
+    allowNull: false, // Password cannot be null
   },
 });
 
-module.exports = User;
+module.exports = User; // Export the User model for querying and manipulating user data
